e5ed16e3910fa3d5c56a4eb6c1f448015085c858
Kopacz Levente <office@imageservices.ro>
2018-01-31 11:06:04 +0200

    Layout brief created

--- app/src/main/java/com/example/android/logicquiz/MainActivity.java
+++ app/src/main/java/com/example/android/logicquiz/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.logicquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void nextPage (View view) {
+        ViewFlipper pageFlipper = (ViewFlipper) findViewById(R.id.main_flipper);
+        pageFlipper.showNext();
+    }
+    public void previousPage (View view) {
+        ViewFlipper pageFlipper = (ViewFlipper) findViewById(R.id.main_flipper);
+        pageFlipper.showPrevious();
     }
 }
