76ef1daf3fc9b56832c17fad4228ab1eea6faa4d
Kopacz Levente <office@imageservices.ro>
2018-01-31 18:58:54 +0200

    Simplified layout

--- app/src/main/java/com/example/android/logicquiz/MainActivity.java
+++ app/src/main/java/com/example/android/logicquiz/MainActivity.java
@@ -4,21 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     // Variable for page number
-    int currentPage = 0;
+    int currentPage = 1;
     ViewFlipper pageFlipper;
-    TextView pageNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pageFlipper = (ViewFlipper) findViewById(R.id.main_flipper);
-        pageNumber = (TextView) findViewById(R.id.page_number);
     }
 
     /**
@@ -26,75 +27,83 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void nextPage (View view) {
+
         currentPage++;
-        // Show/hide the page number and Previous button on last page
-        if (currentPage==5) {
-            hidePageNumber();
-            setNextButtonVisibility(false);
-        } else if (currentPage==1) {
-            setPreviousButtonVisibility(true);
-            updatePageNumber();
-        } else {
-            updatePageNumber();
+        // Hide the page number on last page
+        if (currentPage==6) {
+            hideNextButton();
+            displayPoints(calculatePoints());
+            //showresetButton();
         }
         pageFlipper.showNext();
     }
 
     /**
-     * Flip to the next View
-     * @param view
-     */
-    public void previousPage (View view) {
-        currentPage--;
-        // Hide the page number and Previous button on first page
-        if (currentPage==0) {
-            hidePageNumber();
-            setPreviousButtonVisibility(false);
-        } else if (currentPage==4) {
-            setNextButtonVisibility(true);
-            updatePageNumber();
-        } else {
-            updatePageNumber();
-        }
-        pageFlipper.showPrevious();
-    }
-
-    /**
-     *  Hide the page number on first and last page
-     */
-    private void hidePageNumber() {
-        pageNumber.setVisibility(View.INVISIBLE);
-    }
-
-    /**
-     *  Hide the previous button on first page
-     */
-    private void setPreviousButtonVisibility(boolean visible) {
-        Button previousButton = (Button) findViewById(R.id.previous_button);
-        if (visible) {
-            previousButton.setVisibility(View.VISIBLE);
-        } else {
-            previousButton.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    /**
      *  Hide the next button on last page
      */
-    private void setNextButtonVisibility(boolean visible) {
+    private void hideNextButton() {
         Button nextButton = (Button) findViewById(R.id.next_button);
-        if (visible) {
-            nextButton.setVisibility(View.VISIBLE);
-        } else {
-            nextButton.setVisibility(View.INVISIBLE);
-        }
+        nextButton.setVisibility(View.INVISIBLE);
     }
+    private void displayPoints(int points) {
+        TextView restulText = (TextView) findViewById(R.id.result_text);
+        restulText.setText("" + points);
+    }
+    private int calculatePoints () {
+        int points = 0;
+        // Check the first answer
+        RadioGroup question1RadioGroup = (RadioGroup) findViewById(R.id.question1_radio);
+        int radioButtonID = question1RadioGroup.getCheckedRadioButtonId();
+        View radioButton = question1RadioGroup.findViewById(radioButtonID);
+        int index = question1RadioGroup.indexOfChild(radioButton);
 
-    /**
-     *  Update the page number
-     */
-    private void updatePageNumber() {
-        pageNumber.setText("Page: " + currentPage);
-        pageNumber.setVisibility(View.VISIBLE);
+        if (index == 2) {
+            points += 20;
+        }
+        // Check the second answer
+        RadioGroup question2RadioGroup = (RadioGroup) findViewById(R.id.question2_radio);
+        radioButtonID = question2RadioGroup.getCheckedRadioButtonId();
+        radioButton = question2RadioGroup.findViewById(radioButtonID);
+        index = question2RadioGroup.indexOfChild(radioButton);
+
+        if (index == 1) {
+            points += 20;
+        }
+        // Check the third answers enered value
+        EditText question3EditText = (EditText) findViewById(R.id.danube_capitals);
+        String answer3 = question3EditText.getText().toString();
+
+        if (answer3.equals("4")) {
+            points += 20;
+        }
+
+        // Check the fourth question's checkboxes (+10 point for every correct answer, -10 for every wrong answer)
+        CheckBox question4Checkbox1 = (CheckBox) findViewById(R.id.question4_checkbox1);
+        if (question4Checkbox1.isChecked()) {
+            points -= 10;
+        }
+        CheckBox question4Checkbox2 = (CheckBox) findViewById(R.id.question4_checkbox2);
+        if (question4Checkbox2.isChecked()) {
+            points += 10;
+        }
+        CheckBox question4Checkbox3 = (CheckBox) findViewById(R.id.question4_checkbox3);
+        if (question4Checkbox3.isChecked()) {
+            points -= 10;
+        }
+        CheckBox question4Checkbox4 = (CheckBox) findViewById(R.id.question4_checkbox4);
+        if (question4Checkbox4.isChecked()) {
+            points += 10;
+        }
+        CheckBox question4Checkbox5 = (CheckBox) findViewById(R.id.question4_checkbox5);
+        if (question4Checkbox5.isChecked()) {
+            points += 10;
+        }
+        CheckBox question4Checkbox6 = (CheckBox) findViewById(R.id.question4_checkbox6);
+        if (question4Checkbox6.isChecked()) {
+            points -= 10;
+        }
+
+
+        return points;
     }
 }
